1ebe7c06ed092895b3ea54c978f0e7e86eec13c9
Esther <esther191299@gmail.com>
2021-08-09 02:00:46 +0200

    Initial commit

---
+++ src/main/java/com/tdd_practices/demo/TddPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.tdd_practices.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddPracticesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddPracticesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tdd_practices/demo/TddPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd_practices.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddPracticesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
